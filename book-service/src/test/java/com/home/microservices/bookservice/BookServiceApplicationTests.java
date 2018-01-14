7949faaf54f9b81bd06f7c804fe83a3568705a81
rgavrysh <grome365@gmail.com>
2018-01-14 15:27:21 +0100

    Added tests to book-service. Added jacocoTestReport

--- book-service/src/test/java/com/home/microservices/bookservice/BookServiceApplicationTests.java
+++ book-service/src/test/java/com/home/microservices/bookservice/BookServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.home.microservices.bookservice;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BookServiceApplicationTests {
 
+	@Autowired
+	private BookServiceApplication bookServiceApplication;
+
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(bookServiceApplication != null);
 	}
 
 }
---
+++ book-service/src/test/java/com/home/microservices/bookservice/BooksControllerTests.java
@@ -0,0 +1,46 @@
+package com.home.microservices.bookservice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BooksControllerTests {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void buildMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void whenGetBooks_thenAllFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/books"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isNotEmpty());
+    }
+
+    @Test
+    public void givenBookId_whenGetBook_thenRightFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/books/{bookId}", 1))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Origin"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("Dan Brown"));
+    }
+}
