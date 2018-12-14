98136bca81ddc5334171d29112dcbb12232b7b1d
Roman Gavryshkiv <rgavrysh@macmini506.ddns.softservecom.com>
2018-12-14 18:14:17 +0100

    Added helper scripts

---
+++ discovery/discovery-starter.sh
@@ -0,0 +1,1 @@
+java -Xmx512M -Xms512M -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:/Users/rgavrysh/Java/spring/spring-cloud/discovery/discovery-gc.log -jar build/libs/discovery-0.0.1-SNAPSHOT.jar
---
+++ gateway/gateway-start.sh
@@ -0,0 +1,1 @@
+java -Xmx512M -Xms512M -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:/Users/rgavrysh/Java/spring/spring-cloud/gateway/gateway-gc.log -jar build/libs/gateway-0.0.1-SNAPSHOT.jar
